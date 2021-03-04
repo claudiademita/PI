6762a22c4d7a5b577e425436342c3716ecea4793
claudiademita <56376724+claudiademita@users.noreply.github.com>
2021-03-04 10:54:42 +0700

    Update tugas.cpp
    input 2 variabel dengan for

--- tugas.cpp
+++ tugas.cpp
@@ -3,11 +3,23 @@
 using namespace std;
 
 int pembagian(){
-  int a = 5;
-  int b = 3;
+  int a, b, i;
+  cout<<"masukkan nilai i : ";
+  cin>>i;
+  
+  for(i; i >= 0; i++)
+  {
+  cout<<"masukkan nilai a: ";
+  cin>>a;
+  cout<<endl;
+  
+  cout<<"masukkan nilai b: ";
+  cin>>b;
+  cout<<endl;
   
   cout<<"Hasil : "<<a\b<<endl;
   cout<<endl;
+  }
 }
 
 int main()
