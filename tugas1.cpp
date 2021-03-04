a9ace40a1a1c7b3ffaead0783485c013cec0e4e0
claudiademita <56376724+claudiademita@users.noreply.github.com>
2021-03-04 10:34:51 +0700

    Create tugas1.cpp
    penjumlahan

---
+++ tugas1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int penjumlahan(){
+  int a = 5;
+  int b = 3;
+  
+  cout<<"Hasil : "<<a+b<<endl;
+  cout<<endl;
+}
+
+int main()
+{
+  penjumlahan();
+  return 0;
+}
